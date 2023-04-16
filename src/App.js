7a11a48a7cc4c0d60f6bb2bd056ad905adbb06b5
Esther Ham <youjinee86@yahoo.com>
2023-04-15 22:58:24 -0400

    still trying to fix bugs in the updated code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import ImageList from './components/ImageList';
@@ -10,18 +10,31 @@ function App() {
     const [term, setTerm] = useState('');
     const [page, setPage] = useState(1);
 
+    useEffect(() => {
+        // do nothing
+      }, [page])
+
+    const imageAPI = async () => {
+        console.log('page =', page);
+        const result = await searchImages(term, page);
+        return result;
+    };
+
     const handleSubmit = async (term) => {
         setPage(1);
-        const result = await searchImages(term, 1);
-        setImages(result);
         setTerm(term);
+        const returnedValue = await imageAPI();
+        setImages(returnedValue);
     };
 
     const handleLoadMore = async () => {
         const newPage = page + 1;
-        const result = await searchImages(term, newPage);
-        setImages((prevImages) => [...prevImages, ...result]);
+        console.log('newPage =', newPage);
         setPage(newPage);
+        const returnedValue = await imageAPI();
+        console.log(returnedValue);
+        setImages((prevImages) => [...prevImages, ...returnedValue]);
+        console.log('after API call, page =', page);
     };
 
     return (
